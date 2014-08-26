a9c1483114a8743ec92f450375bd9b08af32da53
monochromegane <dev.kuro.obi@gmail.com>
2014-08-26 19:49:47 +0900

    Added waiting signal function.

--- gear.go
+++ gear.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func Start() {
 	go serve()
+	waitSignal()
 }
 
 func serve() {
@@ -20,6 +24,14 @@ func serve() {
 		return
 	}
 	server.Serve(l)
+}
+
+func waitSignal() {
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGUSR2)
+	for sig := range ch {
+		fmt.Printf("Got a signal %v", sig)
+	}
 }
 
 func dummyHandler(w http.ResponseWriter, r *http.Request) {
